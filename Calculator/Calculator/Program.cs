d223585e37eaf479e80274e17a7a5e2e7c5cfc5c
Ivailo G <ivailogramatikov05@gmail.com>
2015-01-10 22:03:18 +0200

    Commit 1

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Enter first number");
+                Console.Write("num:");
+                int numone = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter symbol");
+                Console.Write("sym:");
+                char charone = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                Console.Write("num:");
+                int numtwo = int.Parse(Console.ReadLine());
+
+                if (charone == '*')
+                {
+                    Console.WriteLine(numone * numtwo);
+                }
+                else if (charone == '/')
+                {
+                    Console.WriteLine(numone / numtwo);
+                }
+                else if (charone == '+')
+                {
+                    Console.WriteLine(numone + numtwo);
+                }
+
+                else if (charone == '-')
+                {
+                    Console.WriteLine(numtwo - numtwo);
+                }
+
+
+            }
+        }
+    }
+}

a714556679006127ce470be73ea8fcb8be759661
Ivailo G <ivailogramatikov05@gmail.com>
2015-01-22 18:21:43 +0200

    Commit 2

---
+++ EasternEgg/Eastern-Egg/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eastern_Egg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine("...........*********........... ");
+                }
+                if (i == 2)
+                {
+                    Console.WriteLine(".........*...........*.........");
+                }
+                if (i == 3)
+                {
+                    Console.WriteLine(".......*...............*.......");
+                }
+                if (i == 5)
+                {
+                    Console.WriteLine(".....*...................*......");
+                }
+                if (i == 6)
+                {
+                    Console.WriteLine("....*.....................*....");
+                }
+                if (i == 7)
+                {
+                    Console.WriteLine("...*.......................*...");
+                }
+                if (i == 8)
+                {
+                    Console.WriteLine("..*.........................*..");
+                }
+                if (i == 9)
+                {
+                    Console.WriteLine("..*@@@@@******@@@******@@@@@*..");
+                }
+                if (i == 10)
+                {
+                    Console.WriteLine("..*@@@@@*****@@@@@*****@@@@@*..");
+                }
+                if (i == 11)
+                {
+                    Console.WriteLine("..*@@@@@****@@@@@@@****@@@@@*..");
+                }
+                if (i == 12)
+                {
+                    Console.WriteLine("..*@@@@@***@@@@@@@@@***@@@@@*..");
+                }
+                if (i == 13)
+                {
+                    Console.WriteLine("..*@@@@@**@@@@@@@@@@@**@@@@@*..");
+                }
+                if (i == 14)
+                {
+                    Console.WriteLine("..*@@@@@@@@@@@@@@@@@@@@@@@@@*..");
+                }
+                if (i == 15)
+                {
+                    Console.WriteLine("...*.......................*...");
+                }
+                if (i == 16)
+                {
+                    Console.WriteLine("....*.....................*....");
+                }
+                if (i == 17)
+                {
+                    Console.WriteLine(".....*..................*......");
+                }
+                if (i == 18)
+                {
+                    Console.WriteLine(".......*...............*.......");
+                }
+                if (i == 19)
+                {
+                    Console.WriteLine(".........*...........*.........");
+                }
+                if (i == 20)
+                {
+                    Console.WriteLine("...........*********...........");
+                }
+            }
+        }
+    }
+}
